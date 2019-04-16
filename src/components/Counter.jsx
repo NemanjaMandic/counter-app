6e2d713d9a80d3408a884ee8c5405e8f9e512f88
NemanjaMandic <nemanjans84@gmail.com>
2019-04-16 12:51:34 +0200

    passing data

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,22 +2,45 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0,
-        imageUrl: 'https://picsum.photos/200'
+        value: this.props.value,
+        tags: ['tag1', 'tag2', 'tag3']
      };
 
-     formatCount = () => {
-         const { count } = this.state;
-         return count === 0 ? 'Zero' : count;
+     formatValue = () => {
+         const { value } = this.state;
+         return value === 0 ? 'Zero' : value;
+     }
+
+     handleIncrement = (product) => {
+         console.log(product);
+         this.setState({
+             value: this.state.value + 1
+         })
+     }
+
+     doHandleIncrement = () => {
+         this.handleIncrement({id: 1})
      }
     render() { 
+        let classes = this.getBadgeClasses();
+
         return ( 
         <div>
-            <img src={this.state.imageUrl} alt="Slidza"/>
-            <span>{this.formatCount()}</span> <button>Increment</button>
+            
+            <span className={classes}>{this.formatValue()}</span> 
+            <button onClick={this.doHandleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+                {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+            </ul>
         </div>
         
         );
+    }
+
+    getBadgeClasses = () => {
+        let classes = "badge m-2 badge-";
+        classes += this.state.value === 0 ? 'warning' : 'primary';
+        return classes;
     }
 }
  
